012985bdc6914885214190d83be180e1bdbc2d11
MD Riaz <mdriaz@alpha.net.bd>
2026-07-05 05:09:41 +0600

    fix: remove broken FIELD widget, use native FIELDS only, bump v0.2.24

--- src/page-layout-tabs/document-template-fields.page-layout-tab.ts
+++ src/page-layout-tabs/document-template-fields.page-layout-tab.ts
@@ -1,10 +1,8 @@
 import { definePageLayoutTab, PageLayoutTabLayoutMode } from 'twenty-sdk/define';
 
 import {
-  DOCUMENT_TEMPLATE_FIELDS,
   DOCUMENT_TEMPLATE_PAGE_LAYOUT_FIELDS_TAB_UNIVERSAL_IDENTIFIER,
   DOCUMENT_TEMPLATE_PAGE_LAYOUT_FIELDS_WIDGET_UNIVERSAL_IDENTIFIER,
-  DOCUMENT_TEMPLATE_PAGE_LAYOUT_HTML_SOURCE_WIDGET_UNIVERSAL_IDENTIFIER,
   DOCUMENT_TEMPLATE_PAGE_LAYOUT_UNIVERSAL_IDENTIFIER,
 } from '../constants/model-identifiers';
 
@@ -20,20 +18,9 @@ export default definePageLayoutTab({
       universalIdentifier: DOCUMENT_TEMPLATE_PAGE_LAYOUT_FIELDS_WIDGET_UNIVERSAL_IDENTIFIER,
       title: 'Fields',
       type: 'FIELDS',
-      gridPosition: { row: 0, column: 0, rowSpan: 4, columnSpan: 4 },
+      gridPosition: { row: 0, column: 0, rowSpan: 8, columnSpan: 4 },
       configuration: {
         configurationType: 'FIELDS',
-      },
-    },
-    {
-      universalIdentifier: DOCUMENT_TEMPLATE_PAGE_LAYOUT_HTML_SOURCE_WIDGET_UNIVERSAL_IDENTIFIER,
-      title: 'HTML Source',
-      type: 'FIELD',
-      gridPosition: { row: 4, column: 0, rowSpan: 4, columnSpan: 4 },
-      configuration: {
-        configurationType: 'FIELD',
-        fieldMetadataId: DOCUMENT_TEMPLATE_FIELDS.htmlSource,
-        fieldDisplayMode: 'FIELD',
       },
     },
   ],
